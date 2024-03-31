127de607671f54726e71fe132a631523df7d9ebc
Parsa Motamedi <parsamotamedi@gmail.com>
2024-03-31 15:47:58 +0200

    new tab icons

--- favicon_links.js
+++ favicon_links.js
@@ -3,5 +3,10 @@ var favicon_links = {
     "brave://password-manager/passwords": "assets/brave.svg",
     "https://www.office.com/": "https://res.cdn.office.net/officehub/images/content/images/favicon_m365-67350a08e8.ico",
     "https://de.scheidt-bachmann.intranet/de/": "https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.scheidt-bachmann.de&size=16",
-    "https://fcs-jira-01.ads.local/login.jsp": "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.jira.com&size=16"
+    "https://fcs-jira-01.ads.local/login.jsp": "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.jira.com&size=16",
+    "https://www.sskm.de/de/home.html": "https://www.sskm.de/content/dam/myif/ssk-muenchen/work/bilder/icons/favicon2x.png",
+    "https://www.sparkasse-moenchengladbach.de/de/home/onlinebanking/nbf/finanzuebersicht.html?n=true": "https://www.sparkasse-moenchengladbach.de/content/dam/myif/ssk-moenchengladbach/work/bilder/icons/favicon2x.png",
+    "https://mein.gebuhrenfrei.com/b2c/de/statements": "https://spcdn001.azurewebsites.net/cdn/touch-icons/favicon-32x32.png",
+    "https://www.alditalk-kundenbetreuung.de/de/": "https://media.medion.com/alditalkapp?impolicy=prod_trans?w=180",
+    "https://homecenter.check24.de/myleads?&state=loggedin&points=true": "https://www.check24.de/favicon.ico"
 };
